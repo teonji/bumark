7185183c3a70f733cff2f964b68bad5db4ca85a1
Matteo Tritto <matteo.tritto@weroad.it>
2022-02-01 22:01:16 +0100

    refactor shared mixin

--- src/components/item.js
+++ src/components/item.js
@@ -1,74 +1,98 @@
-import dayjs from 'dayjs'
-import {ref, defineProps, defineEmits, computed} from 'vue'
-const props = defineProps({
-  id: {
-    type: String,
-    required: true
-  },
-  title: {
-    type: String,
-    required: true
-  },
-  date: {
-    type: String,
-    required: true
-  },
-  tags: {
-    type: Array,
-    required: true
-  },
-  url: {
-    type: String,
-    required: true
-  },
-  settings: {
-    type: Object,
-    required: true
-  },
-  type: {
-    type: String,
-    default: null
-  },
-  provider: {
-    type: String,
-    default: null
-  },
-  description: {
-    type: String,
-    default: null
-  },
-  image: {
-    type: String,
-    default: null
-  },
-  icon: {
-    type: String,
-    default: null
-  },
-  canEdit: {
-    type: Boolean,
-    default: false
-  },
-})
-const dateFormatted = computed(() => dayjs(props.date).format(props.settings.dateFormat))
-const emit = defineEmits(['open', 'remove'])
-const copied = ref(false)
-const open = () => props.canEdit ? emit('open', props.url) : null
-const remove = () => emit('remove', props.id)
-const toggleTag = tag => emit('tag', tag)
-const copy = () => {
-  navigator.clipboard.writeText(props.url)
-  copied.value = true
-  setTimeout(() => {
-    copied.value = false
-  }, 2000)
-}
+import qrcodeVue from 'qrcode.vue'
+import customSelect from '../components/custom-select.vue'
+import { ref } from 'vue'
+
 export default {
-  props,
-  dateFormatted,
-  copied,
-  open,
-  remove,
-  toggleTag,
-  copy,
+  props: {
+    id: {
+      type: String,
+      required: true
+    },
+    title: {
+      type: String,
+      required: true
+    },
+    date: {
+      type: String,
+      required: true
+    },
+    tags: {
+      type: Array,
+      required: true
+    },
+    url: {
+      type: String,
+      required: true
+    },
+    settings: {
+      type: Object,
+      required: true
+    },
+    categories: {
+      type: Array,
+      default: () => []
+    },
+    provider: {
+      type: String,
+      default: null
+    },
+    description: {
+      type: String,
+      default: null
+    },
+    image: {
+      type: String,
+      default: null
+    },
+    icon: {
+      type: String,
+      default: null
+    },
+    category: {
+      type: String,
+      default: null
+    },
+    notes: {
+      type: String,
+      default: null
+    },
+    canEdit: {
+      type: Boolean,
+      default: false
+    },
+  },
+  components: {
+    qrcodeVue,
+    customSelect
+  },
+  emits: ['open', 'remove', 'change', 'tag'],
+  setup (props, { emit }) {
+    const copied = ref(false)
+    const openNotes = ref(false)
+    const openQrCode = ref(false)
+    const open = () => props.canEdit ? emit('open', props.url) : null
+    const remove = () => emit('remove', props.id)
+    const toggleTag = tag => emit('tag', tag)
+    const change = (id, name, value) => emit('change', {id, name, value})
+    const changeCategory = category => change(props.id, 'category', category)
+    const copy = () => {
+      navigator.clipboard.writeText(props.url)
+      copied.value = true
+      setTimeout(() => {
+        copied.value = false
+      }, 2000)
+    }
+
+    return {
+      copied,
+      openNotes,
+      openQrCode,
+      open,
+      remove,
+      toggleTag,
+      change,
+      changeCategory,
+      copy,
+    }
+  }
 }
